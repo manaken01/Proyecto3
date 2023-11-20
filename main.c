efcb07bc40859596b2d6518dd960786bdb01bb8d
manaken01 <82286286+manaken01@users.noreply.github.com>
2023-11-19 18:19:55 -0600

    Update main.c

--- main.c
+++ main.c
@@ -42,6 +42,7 @@ struct listNoDirectory {
 // Puntero a la cabeza de la lista de archivos que estan en el log pero no en el directorio
 struct listNoDirectory* listNoDirectoryHead = NULL;
 struct listNoDirectory* listNoDirectoryEnd = NULL; // Puntero al final de la lista de archivos que estan en el log pero no en el directorio
+int contador;
 
 struct dirent  *dp;
 struct stat     statbuf;
@@ -50,15 +51,48 @@ struct group   *grp;
 struct tm      *tm;
 char            datestring[256];
 
+void liberarListaNoDirectorio() {
+    struct listNoDirectory* current = listNoDirectoryHead;
+    struct listNoDirectory* next;
 
+    while (current != NULL) {
+        next = current->next;
+        free(current);
+        current = next;
+    }
+
+    listNoDirectoryHead = NULL;
+    listNoDirectoryEnd = NULL;
+}
+
+void imprimirListaNoDirectorio() {
+    struct listNoDirectory* current = listNoDirectoryHead;
+
+    while (current != NULL) {
+        printf("Name: %s\nSize: %jd\nDate: %s\n\n", current->file.name, current->file.size, current->file.date);
+        current = current->next;
+    }
+}
 
 void readData() { //Función para leer los datos
     struct fileInfo file;
+    contador = 0;
     FILE *f = fopen("logs.txt", "rb"); //Se abre el archivo de modo que se pueda lockear y leer
     while ((fread(&file, sizeof(struct fileInfo), 1, f)) > 0) {
-            printf("📄 %s - Tamaño: %ld - Creado el: %s\n",  
-            file.name, file.size, file.date); //Imprime el nombre, tamaño y fecha
-        printf("\n");
+        if (strcmp(file.name, "logs.txt") != 0) {
+            struct listNoDirectory* newFile = (struct listNoDirectory*)malloc(sizeof(struct listNoDirectory));
+            snprintf(newFile->file.name, sizeof(newFile->file.name), "%s\n", file.name);
+            snprintf(newFile->file.date, sizeof(newFile->file.date), "%s\n", file.date);
+            newFile->file.size = file.size;
+            if (listNoDirectoryHead == NULL) {
+                listNoDirectoryHead = newFile;
+                listNoDirectoryEnd = newFile;
+            } else {
+                listNoDirectoryEnd->next = newFile;
+                listNoDirectoryEnd = newFile;
+            }
+            contador++;
+        }
     }
     fclose(f);
 }
@@ -90,93 +124,7 @@ void guardarDirectorio(char *dirName) {
     fclose(logs);
 }
 
-void compararDirectorio(int sock, char *dirName){
-    struct 
-
-    DIR *dir = opendir(dirName);
-    FILE* logs = fopen("logs.txt", "rb");
-    if (logs == NULL) {
-        guardarDirectorio(char *dirName);
-        return;
-    }else{
-
-        /* Loop through directory entries. */
-        // En este primer loop se compara por cada archivo del directorio si se encuentra en los logs
-        //Caso 1: si se encuentra el archivo del directorio en los logs pero no se modificó
-        //Caso 1.2: si se encuentra el archivo del directorio en los logs y se modificó
-        //Caso 2: si el archivo que se encuentra en el directorio no se encuentra en los logs es porque es nuevo, se crea el archivo
-        while ((dp = readdir(dir)) != NULL) {
-        struct fileInfo fileD;
-        if (stat(dp->d_name, &statbuf) == -1)
-                continue;
-            if (dp->d_type != DT_DIR) {
-                fileD.size = statbuf.st_size;
-                strncpy(fileD.name, dp->d_name, sizeof(fileD.name) - 1);
-
-                tm = localtime(&statbuf.st_mtime);  // Se inicializa tm con la última fecha de modificación
-                strftime(fileD.date, sizeof(fileD.date), nl_langinfo(D_T_FMT), tm);
-
-                int found = 0;
-                struct fileInfo fileL;
-                while ((fread(&fileL, sizeof(struct fileInfo), 1, logs)) > 0) {
-                    if(strcmp(fileD.name,fileL.name) == 0){
-                        if(strcmp(fileD.date, fileL.date) != 0){
-                            //actualizar archivo
-                        }
-                        found = 1;
-                        
-                    }
-                    
-                }
-                if (found == 0){
-                    //no lo encontro en los logs
-                    // es un archivo nuevo
-                }
-                
-            }
-        }
-
-        //Segundo loop, recorre los logs para encontrar si algun archivo y no existe en el directorio, significa que se eliminó
-
-
-
-        closedir(dir);
-        fclose(logs);
-    }
-}
-//----------- cliente
-int connectoServer(const char** ip){
-  //crear socket para conectar con el servidor y lo retorna
-  int sock;
-  struct sockaddr_in server;
-  
-  #if defined _WIN32
-  WSADATA wsa_data;
-  WSAStartup(MAKEWORD(1, 1), &wsa_data);
-  #endif
-
-  // Create socket
-  sock = socket(AF_INET, SOCK_STREAM, 0);
-  if (sock == -1) {
-    printf("Could not create socket");
-    return -1;
-  }
-  puts("Socket created");
-
-  server.sin_addr.s_addr = inet_addr(ip);
-  server.sin_family = AF_INET;
-  server.sin_port = htons(8889);
-
-  // Connect to remote server
-  if (connect(sock, (struct sockaddr *)&server, sizeof(server)) < 0) {
-    perror("connect failed. Error");
-    return -1;
-  }
-  puts("Connected\n");
-  return sock;
-}
-//-------------
-void startServer() {
+int startServer() {
     int socket_desc, client_sock, c, read_size;
     struct sockaddr_in server, client;
 #if defined _WIN32
@@ -192,7 +140,7 @@ void startServer() {
     // Prepare the sockaddr_in structure
     server.sin_family = AF_INET;
     server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_port = htons(8889);
+    server.sin_port = htons(8888);
     // Bind
     if (bind(socket_desc, (struct sockaddr *)&server, sizeof(server)) < 0) {
         // print the error message
@@ -229,12 +177,11 @@ void startServer() {
 
 int main(int argc, char* argv[]) {
 
-    if (argc == 2) {
+   if (argc == 2) {
         guardarDirectorio(argv[1]);
         readData();
-        startServer();
-    } else if (argc == 3) {
-        compararDirectorio(connectoServer(argv[2]), argv[1]);
+        imprimirListaNoDirectorio();
+        liberarListaNoDirectorio();
     }
 
 }
