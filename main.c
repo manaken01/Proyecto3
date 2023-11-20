dd6cb926706ef8e89a903e3173c2968049168c4e
Mariangeles <mariangcv25@estudiantec.cr>
2023-11-19 18:02:34 -0600

    cambio

--- main.c
+++ main.c
@@ -34,6 +34,15 @@ struct fileInfo {
     char date[256];  // Última fecha de modificación (en formato de cadena)
 };
 
+struct listNoDirectory {
+    struct fileInfo file;
+    struct listNoDirectory* next;
+};
+
+// Puntero a la cabeza de la lista de archivos que estan en el log pero no en el directorio
+struct listNoDirectory* listNoDirectoryHead = NULL;
+struct listNoDirectory* listNoDirectoryEnd = NULL; // Puntero al final de la lista de archivos que estan en el log pero no en el directorio
+
 struct dirent  *dp;
 struct stat     statbuf;
 struct passwd  *pwd;
@@ -41,7 +50,8 @@ struct group   *grp;
 struct tm      *tm;
 char            datestring[256];
 
-<<<<<<< Updated upstream
+
+
 void readData() { //Función para leer los datos
     struct fileInfo file;
     FILE *f = fopen("logs.txt", "rb"); //Se abre el archivo de modo que se pueda lockear y leer
@@ -52,8 +62,6 @@ void readData() { //Función para leer los datos
     }
     fclose(f);
 }
-=======
->>>>>>> Stashed changes
 
 void guardarDirectorio(char *dirName) {
 	DIR *dir = opendir(dirName); //Se abre el directorio 
@@ -82,19 +90,66 @@ void guardarDirectorio(char *dirName) {
     fclose(logs);
 }
 
-<<<<<<< Updated upstream
-int startServer() {
-=======
-void compararDirectorio(int sock){
-    
+void compararDirectorio(int sock, char *dirName){
+    struct 
+
+    DIR *dir = opendir(dirName);
+    FILE* logs = fopen("logs.txt", "rb");
+    if (logs == NULL) {
+        guardarDirectorio(char *dirName);
+        return;
+    }else{
+
+        /* Loop through directory entries. */
+        // En este primer loop se compara por cada archivo del directorio si se encuentra en los logs
+        //Caso 1: si se encuentra el archivo del directorio en los logs pero no se modificó
+        //Caso 1.2: si se encuentra el archivo del directorio en los logs y se modificó
+        //Caso 2: si el archivo que se encuentra en el directorio no se encuentra en los logs es porque es nuevo, se crea el archivo
+        while ((dp = readdir(dir)) != NULL) {
+        struct fileInfo fileD;
+        if (stat(dp->d_name, &statbuf) == -1)
+                continue;
+            if (dp->d_type != DT_DIR) {
+                fileD.size = statbuf.st_size;
+                strncpy(fileD.name, dp->d_name, sizeof(fileD.name) - 1);
+
+                tm = localtime(&statbuf.st_mtime);  // Se inicializa tm con la última fecha de modificación
+                strftime(fileD.date, sizeof(fileD.date), nl_langinfo(D_T_FMT), tm);
+
+                int found = 0;
+                struct fileInfo fileL;
+                while ((fread(&fileL, sizeof(struct fileInfo), 1, logs)) > 0) {
+                    if(strcmp(fileD.name,fileL.name) == 0){
+                        if(strcmp(fileD.date, fileL.date) != 0){
+                            //actualizar archivo
+                        }
+                        found = 1;
+                        
+                    }
+                    
+                }
+                if (found == 0){
+                    //no lo encontro en los logs
+                    // es un archivo nuevo
+                }
+                
+            }
+        }
+
+        //Segundo loop, recorre los logs para encontrar si algun archivo y no existe en el directorio, significa que se eliminó
+
+
+
+        closedir(dir);
+        fclose(logs);
+    }
 }
 //----------- cliente
-int connectoServer(){
+int connectoServer(const char** ip){
   //crear socket para conectar con el servidor y lo retorna
   int sock;
   struct sockaddr_in server;
   
-
   #if defined _WIN32
   WSADATA wsa_data;
   WSAStartup(MAKEWORD(1, 1), &wsa_data);
@@ -108,8 +163,7 @@ int connectoServer(){
   }
   puts("Socket created");
 
-
-  server.sin_addr.s_addr = inet_addr("127.0.0.1");
+  server.sin_addr.s_addr = inet_addr(ip);
   server.sin_family = AF_INET;
   server.sin_port = htons(8889);
 
@@ -123,7 +177,6 @@ int connectoServer(){
 }
 //-------------
 void startServer() {
->>>>>>> Stashed changes
     int socket_desc, client_sock, c, read_size;
     struct sockaddr_in server, client;
 #if defined _WIN32
@@ -177,14 +230,11 @@ void startServer() {
 int main(int argc, char* argv[]) {
 
     if (argc == 2) {
-<<<<<<< Updated upstream
         guardarDirectorio(argv[1]);
         readData();
-=======
         startServer();
     } else if (argc == 3) {
-        compararDirectorio(connectoServer());
->>>>>>> Stashed changes
+        compararDirectorio(connectoServer(argv[2]), argv[1]);
     }
 
 }
